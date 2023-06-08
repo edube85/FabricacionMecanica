cf44a45c53b08863ec87d0d8db92af0124f5f5af
Eduardo <edube85@gmail.com>
2023-06-08 21:41:34 +0200

    Delete src directory

--- src/main/java/com/edusoft/models/dao/IPerfilDao.java
+++
@@ -1,9 +0,0 @@
-package com.edusoft.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.edusoft.entity.Role;
-
-public interface IPerfilDao extends CrudRepository<Role, Long> {
-
-}
--- src/main/java/com/edusoft/models/service/IPerfilesService.java
+++
@@ -1,9 +0,0 @@
-package com.edusoft.models.service;
-
-import com.edusoft.entity.Role;
-
-public interface IPerfilesService {
-	
-	void guardar(Role role);
-
-}
